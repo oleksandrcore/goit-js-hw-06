e7b4c448c32fa7909469acdd84469857e789c880
oleksandrcore <153992542+oleksandrcore@users.noreply.github.com>
2024-06-26 22:23:53 +0300

    complete homework

---
+++ js/task-2.js
@@ -0,0 +1,27 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.items.splice(this.items.indexOf(itemToRemove), 1);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
+console.log(storage.getItems());
+
+storage.addItem("Droid");
+console.log(storage.getItems());
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
